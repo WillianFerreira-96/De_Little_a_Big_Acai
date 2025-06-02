c5e73635bc5254922c094d2514e7dba9c6559b75
Willian Ferreira <willian.ferreira.gomes96@gmail.com>
2025-06-02 17:41:38 -0300

    Limpeza da API

--- API - De Little a Big Açaí/de-litte-a-big-acai/src/main/java/com/litte_acai/de_litte_a_big_acai/repository/ImagemTestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.litte_acai.de_litte_a_big_acai.repository;
-
-import com.litte_acai.de_litte_a_big_acai.model.ImagemTeste;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImagemTestRepository extends JpaRepository<ImagemTeste, Integer> {
-}
