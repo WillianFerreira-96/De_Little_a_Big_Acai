516f4b7c9ba35a4395d441c8f5cd5db3b8f2ac7c
Willian Ferreira <willian.ferreira.gomes96@gmail.com>
2025-06-02 01:30:09 -0300

    feat(api): adiciona o procedimento adicionarItem

--- API - De Little a Big Açaí/de-litte-a-big-acai/src/main/java/com/litte_acai/de_litte_a_big_acai/controller/ItemController.java
+++ API - De Little a Big Açaí/de-litte-a-big-acai/src/main/java/com/litte_acai/de_litte_a_big_acai/controller/ItemController.java
@@ -1,88 +1,54 @@
 package com.litte_acai.de_litte_a_big_acai.controller;
 
-import com.litte_acai.de_litte_a_big_acai.model.Item;
-import com.litte_acai.de_litte_a_big_acai.repository.ItemRepository;
 import com.litte_acai.de_litte_a_big_acai.service.ItemService;
-import jakarta.annotation.Nonnull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.xml.crypto.Data;
 import java.io.IOException;
-import java.sql.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/")
+@RequestMapping(path = "/estoque")
 public class ItemController{
     @Autowired
     ItemService itemService;
 
     @GetMapping(path = "/buscarTodos")
-    private List<Item>getAll(){
+    private List<String>getAll(){
         return itemService.getAll();
     }
 
     @PostMapping(path = "/adicionarItem")
-    private void adicionarItem(@RequestPart MultipartFile imagemItem,
-                               @RequestPart String nomeItem,
-                               @RequestPart String marca,
-                               @RequestPart String descricaoItem,
-                               @RequestPart String categoria,
-                               @RequestPart String precoUni,
-                               @RequestPart String quant,
-                               @RequestPart String volumeUni,
-                               @RequestPart String unidMedida,
-                               @RequestPart Date dataValidade)throws IOException{
-        itemService.addItem(
-                imagemItem,
-                nomeItem,
-                marca,
-                descricaoItem,
-                categoria,
-                precoUni,
-                quant,
-                volumeUni,
-                unidMedida,
-                dataValidade
-        );
-    }
-    /*
-    @PostMapping(path = "/adicionarItem")
-    private void adicionarItem (
+    private ResponseEntity<?> adicionarItem(
         @RequestPart MultipartFile imagemItem,
         @RequestPart String nomeItem,
         @RequestPart String marca,
         @RequestPart String descricaoItem,
         @RequestPart String categoria,
-        @RequestPart Double precoUni,
-        @RequestPart Integer quant,
-        @RequestPart Double volumeUni,
+        @RequestPart String precoUni,
+        @RequestPart String quant,
+        @RequestPart String volumeUni,
         @RequestPart String unidMedida,
+        @RequestPart String dataValidade,
         @RequestPart String lote,
-        @RequestPart String enderecoArmazen) throws IOException{
+        @RequestPart String enderecoArmazen)throws IOException{
 
-
-
-
-        itemService.adicionarItem(
-            imagemItem,
-            nomeItem,
-            marca,
-            descricaoItem,
-            categoria,
-            precoUni,
-            quant,
-            volumeUni,
-            unidMedida,
-            lote,
-            enderecoArmazen
-        );
+         return ResponseEntity.status(HttpStatus.CREATED).body(itemService.addItem(
+                 imagemItem,
+                 nomeItem,
+                 marca,
+                 descricaoItem,
+                 categoria,
+                 precoUni,
+                 quant,
+                 volumeUni,
+                 unidMedida,
+                 dataValidade,
+                 lote,
+                 enderecoArmazen)
+         );
     }
-
-     */
 }
--- API - De Little a Big Açaí/de-litte-a-big-acai/src/main/java/com/litte_acai/de_litte_a_big_acai/service/ItemService.java
+++ API - De Little a Big Açaí/de-litte-a-big-acai/src/main/java/com/litte_acai/de_litte_a_big_acai/service/ItemService.java
@@ -3,13 +3,11 @@ package com.litte_acai.de_litte_a_big_acai.service;
 import com.litte_acai.de_litte_a_big_acai.model.Item;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.xml.crypto.Data;
 import java.io.IOException;
-import java.sql.Date;
 import java.util.List;
 
 public interface ItemService {
-    public void addItem(MultipartFile imagemItem,
+    public Item addItem(MultipartFile imagemItem,
                         String nomeItem,
                         String marca,
                         String descricaoItem,
@@ -18,21 +16,9 @@ public interface ItemService {
                         String quant,
                         String volumeUni,
                         String unidMedida,
-                        Date dataValidade)throws IOException;
-    /*
-    void adicionarItem (MultipartFile imagemItem,
-                        String nomeItem,
-                        String marca,
-                        String descricaoItem,
-                        String categoria,
-                        double precoUni,
-                        int quant,
-                        double volumeUni,
-                        String unidMedida,
+                        String dataValidade,
                         String lote,
-                        String enderecoArmazen) throws IOException;
-    */
-    List<Item>getAll();
+                        String enderecoArmazen)throws IOException;
 
-
+    List<String>getAll();
 }
